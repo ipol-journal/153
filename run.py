e01974ef492ba7162406537b2f3110825e4c5774
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2024-05-10 13:08:01 +0530

    create required tables using tabulate

--- run.py
+++ run.py
@@ -2,6 +2,11 @@
 
 import subprocess
 import argparse
+import sys
+tabulate_path = "/usr/lib/python3/dist-packages"
+sys.path.append(tabulate_path)
+from tabulate import tabulate
+
 
 # parse the arguments
 ap = argparse.ArgumentParser()
@@ -29,6 +34,7 @@ p3 = subprocess.run(['generate_output', 'input_0.png', 'input_1.png', 'transform
                     str(args.robust), str(args.lambd)])
 
 
+#create tables
 with open('transform.mat', 'r') as f, open('input_2.mat', 'r') as f1:
     computed_trans = f.readlines()[1]
     gT_trans = f1.readlines()[1]
@@ -43,7 +49,6 @@ values_equal = computed_trans_values == gt_trans_values
 if values_equal:
     # Compute the absolute difference if both sets of values are equal
     difference = [abs(computed - gT) for computed, gT in zip(computed_trans_values, gt_trans_values)]
-    print("Absolute Difference:", difference)
 else:
     # Assign difference value of -1 if both sets of values are not equal
     difference = "-"
@@ -52,65 +57,63 @@ else:
 # Create header
 head = ["Transformation", "Computed transformation (p)", "Ground truth transformation (p')", "Error |p'-p|"]
 
+if args.transform_type == 8:
+    Transform = "Homography: (h00, h01,..., h21)"
+elif args.transform_type == 4:
+    Transform = "Similarity: (tx,ty,a,b)"
+elif args.transform_type == 3:
+    Transform = "Euclidean: (tx,ty,θ)"
+elif args.transform_type == 2:
+    Transform = "Translation: (tx,ty)"
+elif args.transform_type == 6:
+    Transform = "Affinity: (tx,ty,a00,a01,a10,a11)"
+
+
 # Assign data
 mydata = [
-    ["Homography: (h00, h01,..., h21)", computed_trans, gT_trans, difference]
+    [Transform, computed_trans, gT_trans, difference]
 ]
 
-# Find maximum width for each column
-max_widths = [max(len(str(x)) for x in column) for column in zip(*mydata, head)]
-
-# Display table
 with open('table1.txt', 'w') as file:
-    # Write header
-    file.write("|".join(head) + "\n")
-    file.write("|".join("-" * width for width in max_widths) + "\n")
-
-    # Write data rows
-    for row in mydata:
-        file.write("|".join(str(cell).ljust(width) for cell, width in zip(row, max_widths)) + "\n")
+  file.write(tabulate(mydata, headers=head, tablefmt="grid", numalign="center"))
 
 
-with open('stdout.txt', 'r') as f:
-    content = f.readlines()
-    Time = content[10].split('=')[1]
-    RMSE = content[11].split('=')[1]
-    Error = content[12].split('=')[1]
-    computed_matrix = content[13].split('=')[1].split()
-    gt_matrix = content[14].split('=')[1].split()
+with open('stdout.txt', 'r') as file:
+    content = file.readlines()
+    for line in content:
+        if "Time=" in line:
+            Time = line.split('=')[1].strip()
+        if "d(Hx,H'x)=" in line:
+            Error = line.split('=')[1].strip()
+        if "RMSE=" in line:
+            RMSE = line.split('=')[1].strip()
+        if "Computed Matrix=" in line:
+            computed_matrix = line.split('=')[1].split()
+        if "Original Matrix=" in line:
+            gt_matrix = line.split('=')[1].split()
 
 
 # Create header2
-head = ["", "Added noise", "RMSE", "Error d(Hgtxi,Hxi)", "Time"]
+head2 = ["", "Added noise", "RMSE", "Error d(Hgtxi,Hxi)", "Time"]
 
 # Assign data2
-mydata = [
+mydata2 = [
     ["Results", args.sigma, RMSE, Error, Time]
 ]
 
-# Find maximum width for each column
-max_widths = [max(len(str(x)) for x in column) for column in zip(*mydata, head)]
-
-# Display table2
+#display table
 with open('table2.txt', 'w') as file:
-    # Write header2
-    file.write("|".join(head) + "\n")
-    file.write("|".join("-" * width for width in max_widths) + "\n")
-
-    # Write data2 rows
-    for row in mydata:
-        file.write("|".join(str(cell).ljust(width) for cell, width in zip(row, max_widths)) + "\n")
+  file.write(tabulate(mydata2, headers=head2, tablefmt="grid", numalign="center"))
 
 
 # Format the matrix values
 format_computed_matrix = "\n".join(["\t".join(computed_matrix[i:i+3]) for i in range(0, len(computed_matrix), 3)])
 format_gT_matrix = "\n".join(["\t".join(gt_matrix[i:i+3]) for i in range(0, len(gt_matrix), 3)])
 
-print("Transformation Matrices\n")
-# Print the computed matrix
-print("\033[1mComputed Matrix:\033[0m")
-print(format_computed_matrix)
-
-# Print the computed matrix
-print("\033[1mGround Truth:\033[0m")
-print(format_gT_matrix)
+# Write the formatted matrices to text files
+with open("computed_matrix_file", "w") as file:
+    file.write("Computed Matrix:\n")
+    file.write(format_computed_matrix)
+    file.write("\n\n")
+    file.write("Ground Truth:\n")
+    file.write(format_gT_matrix)
